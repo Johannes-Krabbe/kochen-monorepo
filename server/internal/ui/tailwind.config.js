89f945ebf39588990239d6d3ee2bb3b57cc8f7d5
Johannes Krabbe <github@Krabbe.dev>
2025-09-28 11:17:33 +0200

    continue work

--- server/internal/ui/tailwind.config.js
+++ server/internal/ui/tailwind.config.js
@@ -1,13 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./ui/components/**/*.{html,js,templ}",
-        "./ui/pages/**/*.{html,js,templ}",
+        "./components/**/*.{html,js,templ}",
+        "./pages/**/*.{html,js,templ}",
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Lato', 'sans-serif'],
+            },
+            colors: {
+                'grey': 'hsl(var(--color-grey))',
+                'light-grey': 'hsl(var(--color-light-grey))',
+                'beige': 'hsl(var(--color-beige))',
+                'light-brown': 'hsl(var(--color-light-brown))',
+                'dark-brown': 'hsl(var(--color-dark-brown))',
+                'almost-black': 'hsl(var(--color-almost-black))',
+                'background': 'hsl(var(--background))',
+                'foreground': 'hsl(var(--foreground))',
+                'card': 'hsl(var(--card))',
+                'card-foreground': 'hsl(var(--card-foreground))',
+                'popover': 'hsl(var(--popover))',
+                'popover-foreground': 'hsl(var(--popover-foreground))',
+                'primary': 'hsl(var(--primary))',
+                'primary-foreground': 'hsl(var(--primary-foreground))',
+                'secondary': 'hsl(var(--secondary))',
+                'secondary-foreground': 'hsl(var(--secondary-foreground))',
+                'muted': 'hsl(var(--muted))',
+                'muted-foreground': 'hsl(var(--muted-foreground))',
+                'accent': 'hsl(var(--accent))',
+                'accent-foreground': 'hsl(var(--accent-foreground))',
+                'destructive': 'hsl(var(--destructive))',
+                'destructive-foreground': 'hsl(var(--destructive-foreground))',
+                'border': 'hsl(var(--border))',
+                'input': 'hsl(var(--input))',
+                'ring': 'hsl(var(--ring))',
+            },
+            spacing: {
+                'nav': 'var(--nav-bar-height)',
+            },
+            borderRadius: {
+                DEFAULT: 'var(--radius)',
             },
         },
     },
